dbeab7ecc13ebf73d1a23c84b42c21cd193870a5
AmrAbuElyazid <s-amr.aboelyazid@zewailcity.edu.eg>
2020-07-25 17:50:18 +0200

    Sudo dotnet

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     // PublicTokenKey=14843e0419858c21', 'ClassName');
     // exec('amr');    
     // exec('dotnet /Users/amr/Development/Development/Websites/iprotect-backend/routes/test.dll amr');    
-    $o = shell_exec('dotnet /Users/amr/Downloads/iprotect/test.dll /Users/amr/Downloads/iprotect/test.pdf 123456');  
+    $o = shell_exec('sudo dotnet /Users/amr/Downloads/iprotect/test.dll /Users/amr/Downloads/iprotect/test.pdf 123456');  
     dd($o);  
     // return view('welcome');
 });
