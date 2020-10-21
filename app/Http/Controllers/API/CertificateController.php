670ac7803bb8ff496f0261bce385e6c53db85c82
AmrAbuElyazid <s-amr.aboelyazid@zewailcity.edu.eg>
2020-10-21 03:39:39 +0200

    Certificate all types

--- app/Http/Controllers/API/CertificateController.php
+++ app/Http/Controllers/API/CertificateController.php
@@ -10,6 +10,13 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 
+
+// SIGNER CODE 
+
+    // $dll_path = env('APP_STORAGE_PATH') . 'signer/test.dll';
+    // $file_name = env('APP_STORAGE_PATH') . 'certificates/Kcnc21xxOzBsxk0euq4t1JM5MgCaz9qwlRJHJOMO.jpeg';
+    // $o = shell_exec('sudo dotnet ' . $dll_path . ' ' . $file_name .' 1234567');
+
 class CertificateController extends Controller
 {
     public function newCertificate(Request $request)
@@ -30,18 +37,13 @@ class CertificateController extends Controller
 
     public function upload(Request $request)
     {
-        // return env('APP_STORAGE_PATH');
-        // $disk_gcs = Storage::disk('gcs');
+        $disk_gcs = Storage::disk('gcs');
         $disk_local = Storage::disk('local');
-        // $file = $request->file('file');
-        // $disk_gcs->put('certificates/', $file);
-        // $local = $disk_local->put('certificates/', $file);
+        $file = $request->file('file');
+        $disk_gcs->put('certificates/', $file);
+        $local = $disk_local->put('certificates/', $file);
 
-        $dll_path = env('APP_STORAGE_PATH') . 'signer/test.dll';
-        $file_name = env('APP_STORAGE_PATH') . 'certificates/Kcnc21xxOzBsxk0euq4t1JM5MgCaz9qwlRJHJOMO.jpeg';
-        return 'sudo dotnet ' . $dll_path . ' ' . $file_name .' 123456';
-        $o = shell_exec('sudo dotnet ' . $dll_path . ' ' . $file_name .' 123456');
         return $o;
-
+        
     }
 }
